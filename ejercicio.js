2550f9c5aa094fdcc070ca419cf1652bde2b3862
69misz <132626893+69misz@users.noreply.github.com>
2023-05-26 21:47:13 -0400

    Día 4

---
+++ ejercicio.js
@@ -0,0 +1,27 @@
+//NIVEL 1
+// 1
+let edad = prompt("Ingrese su edad:");
+
+let edad2 =18;
+edad >= 18
+
+? console.log("Tiene la edad suficiente para conducir.")
+: console.log("Te faltan 3 años para conducir.")
+
+// 2
+edad3 = 30
+if (edad3 > 0) {
+    console.log(`${edad3} Eres 5 años mayor que yo.`);
+} 
+
+
+// 3
+let a = 4;
+let b = 3;
+if(a > b) {
+    console.log(`${a} a es mayor que b`);
+} else if(a < b) {
+    console.log(`${b}a es menor que b`);
+}
+
+// 4
